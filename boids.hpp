ad5f65ed3de53e7e1eff99129d88e9d7aec34afe
giacomo_cicala <giacomo.cicala@studio.unibo.it>
2024-05-07 15:18:41 +0200

    problema risolto

---
+++ boids.hpp
@@ -0,0 +1,36 @@
+#ifndef BOIDS_HPP
+#define BOIDS_HPP
+
+#include <cassert>
+#include <vector>
+namespace bds {
+// Classe principale dei boids
+class boids
+{
+ private:
+  std::vector<double> position_{0, 0};
+  std::vector<double> velocity_{0, 0};
+
+ public:
+  boids(double x, double y, double vx, double vy)
+  { // Guarda se va bene costruttore così e se c'è algoritmo
+    position_[0] = x;
+    position_[1] = y;
+    velocity_[0] = vx;
+    velocity_[1] = vy;
+  }
+
+  std::vector<double> position();
+
+  std::vector<double> velocity();
+
+  void update_position(double x, double y);
+};
+
+double abs(boids b1,boids b2);
+
+
+
+} // namespace bds
+
+#endif
