4bd63a39a544ce8e952a4c0163d52acae0ffb481
NicolaAlfieri <nicola.alfieri@studio.unibo.it>
2024-05-07 17:47:54 +0200

    Add files via upload

--- pboid.cpp
+++ pboid.cpp
@@ -1,10 +1,24 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>  //libreria usata solitamente?
 #include <iostream>
+#include <random>
 #include <vector>
 
+class boid {
+ private:
+  std::vector<double> position_{0, 0};
+  std::vector<double> velocity_{0, 0};
+  // Costruttore della classe boids
+ public:
+  boid(double x, double y, double vx, double vy)
+      : position_{x, y}, velocity_{vx, vy} {}
+
+  std::vector<double> position(){return position_;};
+
+  std::vector<double> velocity(){return velocity_;};
+};
 int main() {
-  int n = 10;
+  int n = 10000;
   double d = 5;
   double ds = 4;  // gestire errori di input (mettere catch error)
   double s = 3;   // max vel?
@@ -21,8 +35,18 @@ int main() {
   window.setPosition(sf::Vector2i(0, 0));
   // adegurare a framerate tempo e a grandezza boid
   // Assegnazione delle caratteristiche allo spawn dei boid
-  // !
-  for (int i; i < n; i++) {
+  std::vector<boid> boids;
+  for (int i; i < n; i++) {  
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<> roll_dice1(20, windowWidth - 20);
+    int rand_x = roll_dice1(e1);
+    std::uniform_int_distribution<> roll_dice2(20, windowHeight - 20);
+    int rand_y = roll_dice2(e1);
+    // int rand_vx =
+    // int rand_vy=
+    boid bi{rand_x, rand_y, 0, 0};  //implicita conv double int
+    boids.push_back(bi);
   }
   while (window.isOpen()) {  // un po' buggato sia con opzione schermo intero
                              // che se messo schermo intero dopo
@@ -33,11 +57,13 @@ int main() {
       }
     }
     window.clear(sf::Color::White);
-    sf::CircleShape shape(1);
-    shape.setFillColor(sf::Color::Black);
-    shape.setPosition(50, 50);
-    window.draw(shape);
-
+    for (boid b: boids) {  //passato const& boid
+      sf::CircleShape shape(1);  
+      shape.setFillColor(sf::Color::Black);
+      auto xy = b.position();
+      shape.setPosition(xy[0],xy[1]);
+      window.draw(shape);
+    }
     window.display();
   }
   return 0;
