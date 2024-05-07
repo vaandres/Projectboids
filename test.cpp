eb08538e38841861884fa26325c849ec69ad6b9c
Valerio Andresini <valerio.andresini@studio.unibo.it>
2024-05-07 19:04:57 +0200

    aggiustati nomi

--- test.cpp
+++ test.cpp
@@ -5,13 +5,13 @@
 
 TEST_CASE("Testing dist()")
 {
-  bds::boids b1{1, 2, 3, 4};
-  bds::boids b2{3, 2, 0, 0};
+  bds::boid b1{1, 2, 3, 4};
+  bds::boid b2{3, 2, 0, 0};
 
   auto distance = bds::dist(b1, b2);
   CHECK(distance == 2);
 
-  bds::boids b3{3, 5, 0, 0};
+  bds::boid b3{3, 5, 0, 0};
   distance = bds::dist(b1, b3);
   CHECK(distance == doctest::Approx(3.605).epsilon(0.01));
 }
